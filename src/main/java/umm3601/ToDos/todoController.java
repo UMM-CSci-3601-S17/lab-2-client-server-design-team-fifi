a558bead9485adf5bc08b6efbc66842dfd8da090
Philip Blaskowski <blask017@morris.umn.edu>
2017-01-31 20:59:44 -0600

    Add more functionality to server

--- src/main/java/umm3601/ToDos/todoController.java
+++ src/main/java/umm3601/ToDos/todoController.java
@@ -21,24 +21,49 @@ public class todoController {
     }
 
     public todo[] listTodo(Map<String, String[]> queryParams){
+        todo[] todoTemp = todos;
 
         if(queryParams.containsKey("limit")) {
             int limit = Integer.parseInt(queryParams.get("limit")[0]);
-            todos = limit(todos, limit);
+            todoTemp =  limit(todos, limit);
         }
 
         if(queryParams.containsKey("status")) {
             String status = queryParams.get("status")[0];
 
             if(status.equals("complete")){
-                todos = findComplete(todos, true);
+                todoTemp = findComplete(todos, true);
             }else{
-                todos = findComplete(todos, false);
+                todoTemp = findComplete(todos, false);
             }
         }
 
+        if(queryParams.containsKey("contains")){
+            String body = queryParams.get("contains")[0];
 
-        return todos;
+            todoTemp = bodySearch(todos, body);
+        }
+
+        if(queryParams.containsKey("owner")){
+            String owner = queryParams.get("owner")[0];
+
+            todoTemp = searchOwner(todos, owner);
+        }
+
+        if(queryParams.containsKey("category")){
+            String category = queryParams.get("category")[0];
+
+            todoTemp = searchCategory(todos, category);
+        }
+
+        if(queryParams.containsKey("orderBy")){
+            String parameter = queryParams.get("orderBy")[0];
+
+            todoTemp = order(todos, parameter);
+        }
+
+
+        return todoTemp;
     }
 
     public todo getID(String id){
@@ -77,4 +102,175 @@ public class todoController {
         }
         return actualResult;
     }
+
+    private todo[] bodySearch(todo[] input, String searchParam){
+        ArrayList<todo> holder = new ArrayList<>();
+        for(todo x: todos){
+            if(x.body.contains(searchParam)){
+                holder.add(x);
+            }
+        }
+        todo[] actualResult = new todo[holder.size()];
+        for(int i = 0; i < holder.size(); i++){
+            actualResult[i] = holder.get(i);
+        }
+
+        return actualResult;
+
+    }
+
+    private todo[] searchOwner(todo[] input, String name){
+        ArrayList<todo> holder = new ArrayList<>();
+        for(todo x: todos){
+            if(x.owner.contains(name)){
+                holder.add(x);
+            }
+        }
+        todo[] actualResult = new todo[holder.size()];
+        for(int i = 0; i < holder.size(); i++){
+            actualResult[i] = holder.get(i);
+        }
+
+        return actualResult;
+
+    }
+
+    private todo[] searchCategory(todo[] input, String category){
+        ArrayList<todo> holder = new ArrayList<>();
+        for(todo x: todos){
+            if(x.category.contains(category)){
+                holder.add(x);
+            }
+        }
+        todo[] actualResult = new todo[holder.size()];
+        for(int i = 0; i < holder.size(); i++){
+            actualResult[i] = holder.get(i);
+        }
+
+        return actualResult;
+
+    }
+
+    private todo[] order(todo[] input, String param){
+        todo[] result = new todo[input.length];
+
+        ArrayList<todo> holder = new ArrayList<>();
+
+        for(todo x: input){
+            holder.add(x);
+        }
+
+        if(param.equals("owner")){
+            String[] temp = getNames(input);
+
+            for(int i = 0; i < input.length; i++){
+                String searchParam = temp[i];
+
+                int counter = 0;
+                for(todo x: holder){
+                    if(x.owner.equals(searchParam)){
+                        result[i] = x;
+                        holder.remove(counter);
+                        break;
+                    }
+                    counter++;
+                }
+
+            }
+
+        }else if(param.equals("body")){
+            String[] temp = getBody(input);
+
+            for(int i = 0; i < input.length; i++){
+                String searchParam = temp[i];
+
+                int counter = 0;
+                for(todo x: holder){
+                    if(x.body.equals(searchParam)){
+                        result[i] = x;
+                        holder.remove(counter);
+                        break;
+                    }
+                    counter++;
+                }
+
+            }
+
+        }else if(param.equals("category")){
+            String[] temp = getCategory(input);
+
+            for(int i = 0; i < input.length; i++){
+                String searchParam = temp[i];
+
+                int counter = 0;
+                for(todo x: holder){
+                    if(x.category.equals(searchParam)){
+                        result[i] = x;
+                        holder.remove(counter);
+                        break;
+                    }
+                    counter++;
+                }
+
+            }
+
+        }else{
+            result = getStatus(input);
+
+        }
+
+        return result;
+    }
+
+    private String[] getNames(todo[] input){
+        String[] names = new String[input.length];
+        for(int i = 0; i < input.length; i++){
+            names[i] = input[i].owner;
+        }
+
+        Arrays.sort(names);
+
+        return names;
+    }
+
+    private String[] getBody(todo[] input){
+        String[] comm = new String[input.length];
+        for(int i = 0; i < input.length; i++){
+            comm[i] = input[i].body;
+        }
+
+        Arrays.sort(comm);
+
+        return comm;
+    }
+
+    private String[] getCategory(todo[] input){
+        String[] categ = new String[input.length];
+        for(int i = 0; i < input.length; i++){
+            categ[i] = input[i].category;
+        }
+
+        Arrays.sort(categ);
+
+        return categ;
+    }
+
+    private todo[] getStatus(todo[] input){
+        todo[] result = new todo[input.length];
+        ArrayList<todo> holder = new ArrayList<>();
+
+        for(todo x: findComplete(input,true)){
+            holder.add(x);
+        }
+
+        for(todo x: findComplete(input,false)){
+            holder.add(x);
+        }
+
+        for(int i = 0; i < result.length; i++){
+            result[i] = holder.get(i);
+        }
+        return result;
+    }
+
 }
